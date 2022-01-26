08ffafa4a3425472109e7a51d7f2dfacc3ba4e16
SpinyKarma <samuel.shaw52@gmail.com>
2022-01-26 14:53:20 +0000

    Added docstrings, extended the largest number it could handle, started on a reverse function

--- words-to-int.py
+++ words-to-int.py
@@ -1,4 +1,14 @@
-def multisplit(string, delimiters): #Takes a string and  a list of delimiters and uses the split function sequentially over each delimiter, compiling all remaining strings into a single list
+def multisplit(string, delimiters):
+	"""	Uses the split function over multiple delimiters
+
+		Parameters:
+		string (string): The string to apply the dleimiters to
+		delimiters (list): list of the delimiters to apply to the string
+
+		Returns:
+		newlist (list): A list of strings analagous to the output of the split function
+
+	"""
 	oldlist = [string]
 	for i in range(len(delimiters)):
 		newlist = []
@@ -9,6 +19,16 @@ def multisplit(string, delimiters): #Takes a string and  a list of delimiters an
 	return newlist
 
 dict = {
+	"vigintillion": 1000000000000000000000000000000000000000000000000000000000000000,
+	"novemdecillion": 1000000000000000000000000000000000000000000000000000000000000,
+	"octodecillion": 1000000000000000000000000000000000000000000000000000000000,
+	"septendecillion": 1000000000000000000000000000000000000000000000000000000,
+	"sexdecillion": 1000000000000000000000000000000000000000000000000000,
+	"quindecillion": 1000000000000000000000000000000000000000000000000,
+	"quattuordecillion": 1000000000000000000000000000000000000000000000,
+	"tredecillion": 1000000000000000000000000000000000000000000,
+	"duodecillion": 1000000000000000000000000000000000000000,
+	"undecillion": 1000000000000000000000000000000000000,
 	"decillion": 1000000000000000000000000000000000,
 	"nonillion": 1000000000000000000000000000000,
 	"octillion": 1000000000000000000000000000,
@@ -50,7 +70,18 @@ dict = {
 	"one": 1
 }
 
+tcid = {value : key for (key, value) in dict.items()}
+
 def word2int(words):
+	"""	Takes an integer number written out in English and returns the number it represents
+
+		Parameters:
+		words (string): The written words you want converted
+
+		Returns:
+		fullnum (int): The number representation of the input words
+
+	"""
 	sign = 1
 	if (words[0] == "-"):
 		sign = -1
@@ -70,7 +101,7 @@ def word2int(words):
 			if (list[i] == "a"):
 				list[i] = "one"
 			if (list[i] == "hundred"):
-				tempnum*=100
+				tempnum *= 100
 			elif (list[i] == "thousand" or list[i][-3:] == "ion"):
 				tempnum *= dict[list[i]]
 				fullnum += tempnum
@@ -79,4 +110,39 @@ def word2int(words):
 				tempnum += dict[list[i]]
 	fullnum += tempnum
 	fullnum *= sign
+
 	return fullnum
+
+
+def int2word (number):
+	"""	Takes an integer number and writes it out in English
+
+		Parameters:
+		number (int): The number to be written out
+
+		Returns:
+		words (string): The written out format of the input
+
+	"""
+	number = str(number)
+	i = len(number)
+	list = []
+	while (i>0):
+		if (i<3):
+			temp = number
+			print(number,temp,list)
+			list.insert(0,temp)
+			print(number,temp,list)
+		else:
+			temp = number[-3:]
+			print(number,temp,list)
+			number = number[:-3]
+			print(number,temp,list)
+			list.insert(0,temp)
+			print(number,temp,list)
+		i -= 3
+
+	words = ""
+	return words
+
+int2word(5545566)
